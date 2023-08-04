8fd81b9841a92e35c407b529a5cf05623f0ec036
Roman <romeenice@gmail.com>
2023-08-05 00:31:31 +0300

    fix finish

--- app/i18n.js
+++ app/i18n.js
@@ -1,18 +1,19 @@
-// /utils/translate/i18n.js
 'use client';
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+const backendOptions = {
+  loadPath: (lng) => `./utils/translate/${lng}.json`,
+};
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: 'http://localhost:3000/app/utils/translate/{{lng}}.json',
-    },
+    backend: backendOptions,
     interpolation: {
       escapeValue: false,
     },
--- app/main_screen/Chart.tsx
+++ app/main_screen/Chart.tsx
@@ -1,23 +1,18 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Title from './Title';
+import { useTranslation } from 'react-i18next';
 
-// Generate Sales Data
-function createData(time: string, amount?: number) {
-  return { time, amount };
-}
-
-const data = [
-
-];
+const data = [];
 
 export default function Chart() {
   const theme = useTheme();
+  const { t } = useTranslation();
 
   return (
     <React.Fragment>
-      <Title>Today</Title>
-       тестовий текст
+      <Title>{t('translation.today')}</Title>
+      тестовий текст
     </React.Fragment>
   );
 }
