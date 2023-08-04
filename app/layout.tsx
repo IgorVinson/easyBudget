1c959980a070cd6308268a71dab1435cca49b154
Roman <romeenice@gmail.com>
2023-08-05 01:29:49 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState  } from 'react';
+import React, { useEffect  } from 'react';
 import './globals.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
