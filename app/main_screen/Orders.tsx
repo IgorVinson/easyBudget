a3ae6c8d65bdc4c74cf0b15f6487c33cdf1c9246
Roman <romeenice@gmail.com>
2023-08-06 22:18:29 +0300

    fix

--- app/main_screen/Orders.tsx
+++ app/main_screen/Orders.tsx
@@ -1,131 +1,145 @@
-'use client'
+'use client';
 
 import React, { useState } from 'react';
-import { Table, TableHead, TableBody, TableRow, TableCell, Collapse, IconButton, TextField } from '@mui/material';
+import {
+  Grid,
+  Paper,
+  Typography,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Input,
+  Accordion,
+  AccordionSummary,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-interface Budget {
-  id: string;
-  name: string;
-  status: string;
-  created_at: string;
-  total_planned: number;
-  total_spent: number;
-  categories: Category[];
-}
+const data = [
+  {
+    "id": "budgetId123",
+    "name": "Липень 2023",
+    "status": "active",
+    "created_at": "2023-07-31T00:00:00Z",
+    "total_planned": 6000,
+    "total_spent": 4000,
+    "categories": [
+      {
+        "name": "Продукти",
+        "planned_amount": 5000,
+        "actual_amount": 4900,
+        "categoryId": 1
+      },
+      {
+        "name": "Розваги",
+        "planned_amount": 1000,
+        "actual_amount": 2000,
+        "categoryId": 2
+      }
+    ]
+  },
+  {
+    "id": "budgetId124",
+    "name": "Серпень 2023",
+    "status": "inactive",
+    "created_at": "2023-06-30T00:00:00Z",
+    "total_planned": 7000,
+    "total_spent": 6000,
+    "categories": [
+      {
+        "name": "Одяг",
+        "planned_amount": 3000,
+        "actual_amount": 2900,
+        "categoryId": 3
+      },
+      {
+        "name": "Освіта",
+        "planned_amount": 4000,
+        "actual_amount": 3500,
+        "categoryId": 4
+      }
+    ]
+  },
+  {
+    "id": "budgetId125",
+    "name": "Грудень 2023",
+    "status": "inactive",
+    "created_at": "2023-06-30T00:00:00Z",
+    "total_planned": 4444,
+    "total_spent": 2222,
+    "categories": [
+      {
+        "name": "Одяг",
+        "planned_amount": 222,
+        "actual_amount": 2342,
+        "categoryId": 5
+      },
+      {
+        "name": "Освіта",
+        "planned_amount": 4000,
+        "actual_amount": 3500,
+        "categoryId": 6
+      }
+    ]
+  }
+];
 
-interface Category {
-  name: string;
-  planned_amount: number;
-  actual_amount: number;
-}
+const BudgetAccordion = ({ budgetData }) => {
+  const [expanded, setExpanded] = useState(false);
 
-export default function Orders() {
-  const budgets: Budget[] = [
-    {
-      "id": "budgetId123",
-      "name": "July 2023",
-      "status": "active",
-      "created_at": "2023-07-31T00:00:00Z",
-      "total_planned": 6000,
-      "total_spent": 4000,
-      "categories": [
-        {
-          "name": "Продукти",
-          "planned_amount": 5000,
-          "actual_amount": 4900
-        },
-        {
-          "name": "Розваги",
-          "planned_amount": 1000,
-          "actual_amount": 2000
-        }
-      ]
-    },
-    {
-      "id": "budgetId124",
-      "name": "August 2023",
-      "status": "inactive",
-      "created_at": "2023-06-30T00:00:00Z",
-      "total_planned": 7000,
-      "total_spent": 6000,
-      "categories": [
-        {
-          "name": "Одяг",
-          "planned_amount": 3000,
-          "actual_amount": 2900
-        },
-        {
-          "name": "Освіта",
-          "planned_amount": 4000,
-          "actual_amount": 3500
-        }
-      ]
-    }
-  ];
+  const handleAccordionToggle = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };
 
-  const [open, setOpen] = useState<number[]>([]);
-
-  const handleGroupClick = (monthIndex: number) => {
-    const currentIndex = open.indexOf(monthIndex);
-    const newOpen = [...open];
-    if (currentIndex === -1) {
-      newOpen.push(monthIndex);
-    } else {
-      newOpen.splice(currentIndex, 1);
-    }
-    setOpen(newOpen);
+  const handleInputChange = (categoryId, field, value) => {
+    // логіка зміни даних...
   };
 
   return (
-    <React.Fragment>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>{'Month'}</TableCell>
-            <TableCell>{'Category'}</TableCell>
-            <TableCell>{'Planned Amount'}</TableCell>
-            <TableCell>{'Actual Amount'}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {budgets.map((budget, monthIndex) => (
-            <React.Fragment key={budget.id}>
-              <TableRow>
-                <TableCell>
-                  <IconButton onClick={() => handleGroupClick(monthIndex)}>
-                    {open.includes(monthIndex) ? '-' : '+'}
-                  </IconButton>
-                  {budget.name}
-                </TableCell>
-                <TableCell />
-                <TableCell>{budget.total_planned}</TableCell>
-                <TableCell>{budget.total_spent}</TableCell>
-              </TableRow>
-              {open.includes(monthIndex) &&
-                budget.categories.map((category, categoryIndex) => (
-                  <TableRow key={`${budget.id}-${categoryIndex}`}>
-                    <TableCell>{category.name}</TableCell>
-                    <TableCell />
-                    <TableCell>
-                      <TextField
-                        type="number"
-                        defaultValue={category.planned_amount}
-                        InputProps={{ inputProps: { min: 0 } }}
-                      />
-                    </TableCell>
-                    <TableCell>
-                      <TextField
-                        type="number"
-                        defaultValue={category.actual_amount}
-                        InputProps={{ inputProps: { min: 0 } }}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </React.Fragment>
-          ))}
-        </TableBody>
-      </Table>
-    </React.Fragment>
+    <Accordion expanded={expanded} onChange={handleAccordionToggle}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content">
+        <Typography>{budgetData.name}</Typography>
+      </AccordionSummary>
+      <Grid container component={Paper} style={{ padding: 16 }}>
+        <Grid item xs={12}>
+          <Typography>Категорії</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Table>
+            <TableBody>
+              {budgetData.categories.map((category) => (
+                <TableRow key={category.categoryId}>
+                  <TableCell>{category.name}</TableCell>
+                  <TableCell>
+                    <Input
+                      type="number"
+                      value={category.planned_amount}
+                      onChange={(e) => handleInputChange(category.categoryId, 'planned_amount', e.target.value)}
+                    />
+                    <Input
+                      type="number"
+                      value={category.actual_amount}
+                      onChange={(e) => handleInputChange(category.categoryId, 'actual_amount', e.target.value)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Grid>
+      </Grid>
+    </Accordion>
+  );
+};
+
+export default function Orders() {
+  return (
+    <Grid container spacing={2}>
+      {data.map((item) => (
+        <Grid item xs={12} key={item.id}>
+          <BudgetAccordion budgetData={item} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
