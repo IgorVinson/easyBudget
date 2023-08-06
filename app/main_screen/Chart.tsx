dc43b9d415b0bc2ac1eeb5ae444978f785e483de
IgorVinson <igorvinson@gmail.com>
2023-08-06 17:30:33 -0400

    creating form for adding budgets categories

--- app/main_screen/Chart.tsx
+++ app/main_screen/Chart.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Title from './Title';
 import { useTranslation } from 'react-i18next';
 
-const data = [];
-
 export default function Chart() {
-  const theme = useTheme();
   const { t } = useTranslation();
 
   return (
     <React.Fragment>
       <Title>{t('translation.today')}</Title>
-      тестовий текст
     </React.Fragment>
   );
 }
