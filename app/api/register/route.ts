97d5845bde599100b034b4da6008efa2071431ea
IgorVinson <igorvinson@gmail.com>
2023-08-14 12:30:23 -0400

    upgrade set up for mongo and prisma, add instruction, create test and registration route

---
+++ app/api/register/route.ts
@@ -0,0 +1,38 @@
+import {NextResponse} from 'next/server'
+import {PrismaClient} from "@prisma/client";
+const bcrypt = require('bcrypt')
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+    try {
+        const {username, email, password} = await request.json()
+
+        const existingUser = await prisma.user.findUnique({
+            where: {email},
+        })
+
+        if (existingUser) {
+            NextResponse.json(
+                {message: "Email already in use"},
+                {status: 400,})
+            return
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                hash: hashedPassword,
+                username: `${username}`,
+                createdAt: new Date(),
+                language: "en",
+            },
+        })
+        return NextResponse.json({user}, {status: 201})
+    } catch (e) {
+        return NextResponse.json({message: e.message}, {status: 500})
+    }
+}
+
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,7 +4,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            username: true,
+            createdAt: true,
+            avatar: true,
+            email: true,
+            language: true,
+        },
+    });
     return NextResponse.json(users)
 }
 
